b4393f84d3c8b8b51f17b12b87f37585ecd97041
Pascal Knoth <pknoth@octo.com>
2018-03-21 11:18:00 +0100

    new schema integration

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,158 +3,24 @@ require 'zip'
 
 class Enrollment < ApplicationRecord
   DOCUMENT_TYPES = %w[
-    Document::CNILVoucher
-    Document::CertificationResults
-    Document::FranceConnectCompliance
-    Document::LegalBasis
-  ].freeze
-  SECURITY_DOCUMENT_TYPES = %w[
-    Document::ProductionCertificatePublicKey
-    Document::CertificationAuthorityPublicKey
   ].freeze
 
   resourcify
   has_many :messages
-
-  validate :agreement_validation
-  validate :applicant_validation
-  validate :step_1
-
-  before_save :clean_json
-  after_save :applicant_workflow
-
   has_many :documents
   accepts_nested_attributes_for :documents
 
+  validate :convention_validated?
+
   # Note convention on events "#{verb}_#{what}" (see CoreAdditions::String#as_event_personified)
-  state_machine :state, initial: 'filled_application' do
-    state 'filled_application'
-    state 'waiting_for_approval' do
-      validate :cnil_voucher_detail_validation
-      validate :certification_results_detail_validation
-      validate :document_validation
-
-      def cnil_voucher_detail_validation
-        errors.add(:cnil_voucher_detail, "CNIL : la référence de l'avis doit être rempli") unless cnil_voucher_detail['reference'].present?
-        errors.add(:cnil_voucher_detail, "CNIL : la formalité CNIL doit être remplie") unless cnil_voucher_detail['formality'].present?
-      end
-
-      def certification_results_detail_validation
-        errors.add(:certification_results_detail, "HOMOLOGATION : le nom de l'autorité de certification doit être rempli") unless certification_results_detail['name'].present?
-        errors.add(:certification_results_detail, "HOMOLOGATION : la fonction de l'autorité de certification doit être remplie") unless certification_results_detail['position'].present?
-        errors.add(:certification_results_detail, "HOMOLOGATION : la date d'homologation doit être remplie") unless certification_results_detail['start'].present?
-        errors.add(:certification_results_detail, "HOMOLOGATION : la durée d'homologation doit être remplie") unless certification_results_detail['duration'].present?
-      end
-
-      def document_validation
-        unless DOCUMENT_TYPES.all? do |document|
-          documents.where(type: document).present?
-        end
-        errors.add(:documents, 'Vous devez envoyer tous les documents demandés')
-        end
-      end
-    end
-    state 'application_approved'
-    state 'technical_validation' do
-      validates :applicant, presence: true
-    end
-    state 'application_ready' do
-      validate :technical_validation
-      validate :document_validation
-
-      after_save :create_archive
-
-      def document_validation
-        unless SECURITY_DOCUMENT_TYPES.all? do |document|
-          documents.where(type: document).present?
-        end
-          errors.add(:documents, 'Vous devez envoyer tous les documents demandés')
-        end
-      end
-
-      def technical_validation
-        errors.add(:production_ips, 'Vous devez fournir les IPs de production') unless production_ips.present?
-      end
-
-      def create_archive
-        zip_file = Rails.root.join("tmp/#{SecureRandom.hex}.zip")
-
-        documents_to_zip = documents.select { |e| SECURITY_DOCUMENT_TYPES.include?(e.type) }
-
-        Zip::File.open(zip_file, Zip::File::CREATE) do |zipfile|
-          documents_to_zip.each do |document|
-            zipfile.add("#{SecureRandom.hex}-#{File.basename(document.attachment.path)}", document.attachment.file.path)
-          end
-          zipfile.get_output_stream("production_ips.txt") { |f| f.write production_ips }
-        end
-
-        documents.create(
-          type: 'Document::SecurityArchive',
-          attachment: zip_file.open
-        )
-        FileUtils.rm(zip_file)
-      end
-    end
-    state 'deployed'
-
-    after_transition %w[filled_application completed_application] => 'waiting_for_approval' do |enrollment, transition|
-      enrollment.messages.create(
-        content: 'votre dossier a été complèté'
-      )
-    end
-    event 'complete_application' do
-      transition %w[filled_application completed_application] => 'waiting_for_approval'
-    end
-
-    event 'send_application' do
-      transition 'completed_application' => 'waiting_for_approval'
-    end
-
-    before_transition 'waiting_for_approval' => 'filled_application' do |enrollment, transition|
-      enrollment.messages.create(content: 'Votre dossier a été refusé')
-    end
-    event 'refuse_application' do
-      transition %w[filled_application waiting_for_approval] => 'filled_application'
-    end
-
-    after_transition any => 'application_approved' do |enrollment, transition|
-      enrollment.messages.create(
-        content: 'votre dossier a été approuvé'
-      )
-    end
-    event 'approve_application' do
-      transition %w[filled_application completed_application waiting_for_approval] => 'application_approved'
-    end
-
-    after_transition any => 'technical_validation' do |enrollment, transition|
-      enrollment.messages.create(
-        content: 'vous avez signé la convention',
-      )
-    end
-    event 'sign_convention' do
-      transition 'application_approved' => 'technical_validation'
-    end
-
-    after_transition any => 'application_ready' do |enrollment, transition|
-      enrollment.messages.create(
-        content: "vos données de sécurité sont en cours d'intégration",
-      )
-    end
-    event 'deploy_security' do
-      transition %w[application_ready technical_validation] => 'application_ready'
-    end
-
-    after_transition any => 'deployed' do |enrollment, transition|
-      enrollment.messages.create(
-        content: 'votre application est prête pour la mise en production',
-      )
-    end
-    event 'deploy_application' do
-      transition 'application_ready' => 'deployed'
-    end
+  state_machine :state, initial: :pending do
   end
 
   private
+
+  def convention_validated?
+    errors[:validation_de_convention] << "Vous devez valider la convention avant de continuer" unless validation_de_convention?
+  end
 
   def clean_json
     self.service_description = _clean_json(service_description)
--- app/policies/enrollment_policy.rb
+++ app/policies/enrollment_policy.rb
@@ -2,60 +2,15 @@
 
 class EnrollmentPolicy < ApplicationPolicy
   def create?
-    return false unless user.france_connect?
-    true
+    user.service_provider?
   end
 
   def update?
-    res = false
-    res = true if record.can_complete_application?
-    res = record.applicant&.fetch('email', nil).present? if record.can_sign_convention?
-    res = true if record.can_deploy_security?
-    res && user.france_connect?
+    user.has_role?(:applicant, record)
   end
 
   def convention?
-    record.can_sign_convention? || record.can_deploy_security? || record.can_deploy_application? || record.deployed?
-  end
-
-  def complete_application?
-    user.france_connect? && record.can_complete_application?
-  end
-
-  def show_domain?
-    user.france_connect? ||
-      (user.dgfip? && user.oauth_roles.include?('domain'))
-  end
-
-  def approve_application?
-    user.dgfip? && user.oauth_roles.include?('domain') && record.can_approve_application?
-  end
-
-  def refuse_application?
-    user.dgfip? && user.oauth_roles.include?('domain') && record.can_refuse_application?
-  end
-
-  def sign_convention?
-    user.france_connect? && record.can_sign_convention?
-  end
-
-  def edit_security?
-    user.france_connect? && record.can_deploy_security?
-  end
-
-  def show_security?
-    (
-      user.france_connect? ||
-      (user.dgfip? && user.oauth_roles.include?('security'))
-    ) && %w[application_ready deployed].include?(record.state)
-  end
-
-  def deploy_security?
-    user.france_connect? && record.can_deploy_security?
-  end
-
-  def deploy_application?
-    user.dgfip? && user.oauth_roles.include?('security') && record.can_deploy_application?
+    false
   end
 
   class Scope < Scope
