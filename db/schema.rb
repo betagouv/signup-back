d797b8547f0a42d31d331f87bb1fee36dc3664bc
Pascal Knoth <pknoth@octo.com>
2018-03-28 08:24:09 +0200

    examples of code added to scopes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323115633) do
+ActiveRecord::Schema.define(version: 20180327155316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20180323115633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.json "services", default: [], array: true
+    t.string "node_example"
   end
 
   create_table "users", force: :cascade do |t|
@@ -109,7 +110,6 @@ ActiveRecord::Schema.define(version: 20180323115633) do
     t.string "provider"
     t.string "uid"
     t.string "oauth_roles", default: [], array: true
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
