7484a4f4fad5cabd4aa6da291347db60dbb7b766
Hugo Lepetit <giglemad@gmail.com>
2018-06-03 10:37:29 +0200

    [RSPEC] Cleanup in rspec boot files, added factory girl syntax methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,9 @@ require 'simplecov'
 SimpleCov.start
 
 RSpec.configure do |config|
+  # FactoryBot inclusion of methods (create, build etc.)
+  config.include FactoryGirl::Syntax::Methods
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
