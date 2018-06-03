70afa8043c8901663cf42a68adbc269e0d8447c3
Hugo Lepetit <giglemad@gmail.com>
2018-06-03 11:15:19 +0200

    [SPEC] Cleaned clutter user factories that added no value

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,17 +4,5 @@ FactoryGirl.define do
   factory :user do
     email { nano_timestamp_string }
     uid   { nano_timestamp_string }
-
-    factory :user_dgfip do
-      provider 'dgfip'
-    end
-
-    factory :user_api_particulier do
-      provider 'api_particulier'
-    end
-
-    factory :user_api_entreprise do
-      provider 'api_entreprise'
-    end
   end
 end
--- spec/support/spec_policy_helper.rb
+++ spec/support/spec_policy_helper.rb
@@ -4,8 +4,16 @@ module SpecPolicyHelper
       record_factories.each do |record_factory|
         user_factories.each do |user_factory, allowed|
           describe "with a #{user_factory} user and a #{record_factory} record" do
-            let(:record) { FactoryGirl.create(record_factory) }
-            let(:user) { FactoryGirl.create(user_factory) }
+
+            #XXX temporary hack
+            # legacy of dependency on factory names that were like factory_#{provider} and
+            # do nothing but add set provider value to the suffix
+
+            provider = (user_factory.to_s.split('_') - ['user']).join('_')
+            provider = (provider.empty?) ? nil : provider
+
+            let(:user)    { create(:user, provider: provider) }
+            let(:record)  { create(record_factory) }
 
             if allowed
               it 'allow access' do
