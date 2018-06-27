3d0f145288381219afaef686088f2c8540192925
Pascal Knoth <pknoth@octo.com>
2018-06-27 15:50:23 +0200

    strong params (whitelists) and expose technical_inputs data
    rebase fix

--- app/policies/enrollment_policy.rb
+++ app/policies/enrollment_policy.rb
@@ -6,7 +6,27 @@ class EnrollmentPolicy < ApplicationPolicy
           messages_attributes: [:content]
       },
       'send_technical_inputs' => [
-        :ips_de_production
+        :autorite_certification,
+        :ips_de_production,
+        :autorite_certification_nom,
+        :autorite_certification_fonction,
+        :date_homologation,
+        :date_fin_homologation,
+        :nombre_demandes_annuelle,
+        :pic_demandes_par_seconde,
+        :nombre_demandes_mensuelles_jan,
+        :nombre_demandes_mensuelles_fev,
+        :nombre_demandes_mensuelles_mar,
+        :nombre_demandes_mensuelles_avr,
+        :nombre_demandes_mensuelles_mai,
+        :nombre_demandes_mensuelles_jui,
+        :nombre_demandes_mensuelles_jul,
+        :nombre_demandes_mensuelles_aou,
+        :nombre_demandes_mensuelles_sep,
+        :nombre_demandes_mensuelles_oct,
+        :nombre_demandes_mensuelles_nov,
+        :nombre_demandes_mensuelles_dec,
+        :recette_fonctionnelle
       ]
   }
 
