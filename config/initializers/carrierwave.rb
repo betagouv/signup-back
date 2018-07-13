06159c67996efce5430e0b993ecfa53a8f5d071e
Pascal Knoth <pknoth@octo.com>
2018-07-13 17:33:27 +0200

    add fog config for ovh openstack

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,14 @@
 CarrierWave.configure do |config|
-  config.root = Rails.root.join('..')
+  config.fog_provider = 'fog/openstack'
+  config.fog_credentials = {
+    provider: 'OpenStack',
+    openstack_tenant: "",
+    openstack_api_key: "",
+    openstack_username: "",
+    openstack_auth_url: "https://auth.cloud.ovh.net/v2.0/tokens",
+    openstack_region: ""
+  }
+  config.fog_directory = "signup-uploads"
+  config.fog_public = false
+  config.storage = :fog
 end
