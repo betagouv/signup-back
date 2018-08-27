557d7d20ddfa3b635215d158e0c9e57376fef04a
Raphaël Dubigny <rdubigny@gmail.com>
2018-08-27 12:19:08 +0200

    add more information in send application email + send only one email instead of 2

--- app/mailers/enrollment_mailer.rb
+++ app/mailers/enrollment_mailer.rb
@@ -1,17 +1,18 @@
-class EnrollmentMailer < ApplicationMailer
-  default from: 'contact@particulier.api.gouv.fr', charset: 'UTF-8'
+class EnrollmentMailer < ActionMailer::Base
+  default from: %("API Particulier" <contact@particulier.api.gouv.fr>), charset: 'UTF-8'
 
   %i[send_application].each do |action|
     define_method(action) do
       recipients = enrollment.other_party(user).map(&:email)
 
       return unless recipients.present?
-      mail(to: recipients) do |format|
-        format.html { render inline: I18n.t("enrollment_mailer.#{action}.content"), layout: 'enrollment', charset: 'UTF-8' }
-        format.text { render inline: I18n.t("enrollment_mailer.#{action}.content"), layout: 'enrollment', charset: 'UTF-8' }
-      end
+
+      @email = user.email
+      @url = "#{ENV.fetch('FRONT_HOST')}/#{enrollment.fournisseur_de_donnees}/#{enrollment.id}"
+      mail(to: recipients, subject: 'Nouvelle demande sur signup.api.gouv.fr')
     end
   end
+
   private
 
   def user
