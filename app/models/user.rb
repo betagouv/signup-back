be5c3c4def722a5b26d794c97e7429336ea8d7e8
Raphaël Dubigny <rdubigny@gmail.com>
2018-09-05 15:12:21 +0200

    first working implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   devise :omniauthable, omniauth_providers: %i[resource_provider france_connect]
 
   rolify
