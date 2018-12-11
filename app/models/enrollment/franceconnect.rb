de0896bebdf3c5a624b7739c09ea39ca906bec18
Raphaël Dubigny <rdubigny@gmail.com>
2018-12-11 12:08:24 +0100

    add data in fc form

--- app/models/enrollment/franceconnect.rb
+++ app/models/enrollment/franceconnect.rb
@@ -1,3 +1,11 @@
 class Enrollment::Franceconnect < Enrollment
   resourcify
+
+  protected
+
+  def sent_validation
+    super
+    errors[:donnees] << "Vous devez renseigner la conservation des données avant de continuer" unless donnees && donnees['conservation'].present?
+    errors[:donnees] << "Vous devez renseigner les destinataires des données avant de continuer" unless donnees && donnees['destinataires'].present?
+  end
 end
