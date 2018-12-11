0a2118bd2e5d61501eb8ba742ef00681d64f6f55
Raphaël Dubigny <rdubigny@gmail.com>
2018-12-11 20:18:07 +0100

    admin can download attachement

--- app/policies/document_policy.rb
+++ app/policies/document_policy.rb
@@ -2,7 +2,7 @@
 
 class DocumentPolicy < ApplicationPolicy
   def show?
-    return true if user.dgfip?
+    return true if user.provided_by?(record.attachable.resource_provider)
     return false unless user.service_provider?
     user.has_role?(:applicant, record.attachable)
   end
