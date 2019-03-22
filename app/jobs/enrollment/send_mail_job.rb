d9ec45655326c7af49667badbc3999231929634f
Raphaël Dubigny <rdubigny@gmail.com>
2019-03-22 12:25:46 +0100

    do not fail silently when send mail errored

--- app/jobs/enrollment/send_mail_job.rb
+++ app/jobs/enrollment/send_mail_job.rb
@@ -2,10 +2,6 @@ class Enrollment::SendMailJob < ApplicationJob
   queue_as :default
 
   def perform(enrollment, user, event)
-    begin
-      EnrollmentMailer.with(user: user, enrollment: enrollment).send(event).deliver_now
-    rescue NoMethodError => e
-      # if no mail is defined for this action we just silently fail
-    end
+    EnrollmentMailer.with(user: user, enrollment: enrollment).send(event).deliver_now
   end
 end
