c61126913a6820d88ddc0c7b87b633ee4dcc6b8b
Raphaël Dubigny <rdubigny@gmail.com>
2019-08-08 14:46:36 +0200

    notify rgpd contact when a enrollment is validated

--- app/mailers/enrollment_mailer.rb
+++ app/mailers/enrollment_mailer.rb
@@ -1,54 +1,56 @@
 class EnrollmentMailer < ActionMailer::Base
-  default charset: 'UTF-8'
+  default charset: "UTF-8"
 
   SUBJECTS = {
-      'send_application' => "Nous avons bien reçu votre demande d'accès",
-      'validate_application' => 'Votre demande a été validée',
-      'review_application' => 'Votre demande requiert des modifications',
-      'refuse_application' => 'Votre demande a été refusée',
-      'notify_application_sent' => 'Nouvelle demande sur signup.api.gouv.fr',
-      'create_application' => 'Votre demande a été enregistrée'
+    'send_application' => "Nous avons bien reçu votre demande d'accès",
+    'validate_application' => "Votre demande a été validée",
+    'review_application' => "Votre demande requiert des modifications",
+    'refuse_application' => "Votre demande a été refusée",
+    'notify_application_sent' => "Nouvelle demande sur signup.api.gouv.fr",
+    'notify_application_validated' => "Nouvelle demande d'habilitation api.gouv.fr",
+    'create_application' => "Votre demande a été enregistrée",
   }
 
   MAIL_PARAMS = {
     'franceconnect' => {
-      'sender' => 'support.partenaires@franceconnect.gouv.fr',
-      'target_api' => 'FranceConnect'
+      'sender' => "support.partenaires@franceconnect.gouv.fr",
+      'target_api' => "FranceConnect",
     },
     'dgfip' => {
-      'sender' => 'contact@api.gouv.fr',
-      'target_api' => 'API « impôt particulier »'
+      'sender' => "contact@api.gouv.fr",
+      'target_api' => "API « impôt particulier »",
     },
     'api_particulier' => {
-      'sender' => 'contact@particulier.api.gouv.fr',
-      'target_api' => 'API Particulier'
+      'sender' => "contact@particulier.api.gouv.fr",
+      'target_api' => "API Particulier"
     },
     'api_droits_cnam' => {
-      'sender' => 'contact@api.gouv.fr',
-      'target_api' => 'API CNAM'
+      'sender' => "contact@api.gouv.fr",
+      'target_api' => "API CNAM",
     },
     'api_entreprise' => {
-      'sender' => 'contact@api.gouv.fr',
-      'target_api' => 'API Entreprise'
+      'sender' => "contact@api.gouv.fr",
+      'target_api' => "API Entreprise",
     },
     'preuve_covoiturage' => {
-      'sender' => 'contact@covoiturage.beta.gouv.fr',
-      'target_api' => 'Registre de preuve de covoiturage'
+      'sender' => "contact@covoiturage.beta.gouv.fr",
+      'target_api' => "Registre de preuve de covoiturage",
     }
   }
 
   def notification_email
-    @target_api_label = MAIL_PARAMS[params[:target_api]]['target_api']
+    @target_api_label = MAIL_PARAMS[params[:target_api]]["target_api"]
     @message = params[:message]
     @applicant_email = params[:applicant_email]
+    @rgpd_role = params[:rgpd_role]
 
-    @url = "#{ENV.fetch('FRONT_HOST')}/#{params[:target_api].gsub(/_/, '-')}/#{params[:enrollment_id]}"
+    @url = "#{ENV.fetch("FRONT_HOST")}/#{params[:target_api].gsub(/_/, '-')}/#{params[:enrollment_id]}"
     mail(
-        # The list of emails can be an array of email addresses or a single string with the addresses separated by commas.
-        to: params[:to],
-        subject: SUBJECTS[params[:template]],
-        from: MAIL_PARAMS[params[:target_api]]['sender'],
-        template_name: params[:template]
+      # The list of emails can be an array of email addresses or a single string with the addresses separated by commas.
+      to: params[:to],
+      subject: SUBJECTS[params[:template]],
+      from: MAIL_PARAMS[params[:target_api]]["sender"],
+      template_name: params[:template]
     )
   end
 end
