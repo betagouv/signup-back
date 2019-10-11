7a53645202e4638b54b3abbbaaf0b7881f915de1
tmerlier <tmerlier@student.42.fr>
2019-10-11 11:26:11 +0200

    Makes all fields of the second dgfip form mandatory

--- app/models/enrollment/api_impot_particulier_step2.rb
+++ app/models/enrollment/api_impot_particulier_step2.rb
@@ -9,9 +9,27 @@ class Enrollment::ApiImpotParticulierStep2 < Enrollment
   end
 
   def sent_validation
+    # Form
     errors[:siret] << "Vous devez renseigner un SIRET d'organisation valide avant de continuer" unless nom_raison_sociale.present?
+
+    # Homologation de securite
+    errors[:autorite_homologation_nom] << "Vous devez renseigner le nom de l'autorité d'homologation avant de continuer" unless additional_content&.fetch('autorite_homologation_nom', false)&.present?
+    errors[:autorite_homologation_fonction] << "Vous devez renseigner la fonction de l'autorité d'homologation avant de continuer" unless additional_content&.fetch('autorite_homologation_fonction', false)&.present?
+    errors[:date_homologation] << "Vous devez renseigner la date de début de l’homologation avant de continuer" unless additional_content&.fetch('date_homologation', false)&.present?
+    errors[:date_fin_homologation] << "Vous devez renseigner la date de fin de l’homologation avant de continuer" unless additional_content&.fetch('date_fin_homologation', false)&.present?
+    errors[:documents_attributes] << "Vous devez joindre le document de décision d’homologation avant de continuer" unless documents.where(type: "DecisionHomologation").present?
+
+    # Entrant technique
     errors[:ips_de_production] << "Vous devez renseigner les IP(s) de production avant de continuer" unless additional_content&.fetch('ips_de_production', false)&.present?
+
+    # Volumetrie
+    errors[:nombre_demandes_annuelle] << "Vous devez renseigner le nombre de demandes annuelle avant de continuer" unless additional_content&.fetch('nombre_demandes_annuelle', false)&.present?
+    errors[:pic_demandes_par_seconde] << "Vous devez renseigner le nombre de demandes mensuel par heure avant de continuer" unless additional_content&.fetch('pic_demandes_par_seconde', false)&.present?
+    errors[:nombre_demandes_mensuelles] << "Vous devez renseigner le nombre de demandes mensuel pour chaque mois avant de continuer" unless additional_content&.fetch('nombre_demandes_mensuelles', false)&.present&.lenght == 12
+
+    # Recette fonctionnelle
     errors[:recette_fonctionnelle] << "Vous devez attester avoir réaliser une recette fonctionnelle avant de continuer" unless additional_content&.fetch('recette_fonctionnelle', false)&.present?
+
     unless user.email_verified
       errors[:base] << "Vous devez activer votre compte api.gouv.fr avant de continuer.
 Merci de cliquer sur le lien d'activation que vous avez reçu par mail.
