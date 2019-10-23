4228addce7a2a455a9e7d045be6e8f0fdbecc6ed
tmerlier <tmerlier@student.42.fr>
2019-10-23 17:00:01 +0200

    Replace apostrophe

--- app/models/enrollment/api_impot_particulier_step2.rb
+++ app/models/enrollment/api_impot_particulier_step2.rb
@@ -4,17 +4,17 @@ class Enrollment::ApiImpotParticulierStep2 < Enrollment
   def update_validation
     errors[:linked_franceconnect_enrollment_id] << "Vous devez associer cette demande à une demande Franceconnect validée" unless linked_franceconnect_enrollment_id.present?
     # the following 2 errors should never occur #defensiveprogramming
-    errors[:target_api] << "Une erreur inattendue est survenue: pas d'API cible" unless target_api.present?
-    errors[:organization_id] << "Une erreur inattendue est survenue: pas d'organisation" unless organization_id.present?
+    errors[:target_api] << "Une erreur inattendue est survenue: pas d’API cible" unless target_api.present?
+    errors[:organization_id] << "Une erreur inattendue est survenue: pas d’organisation" unless organization_id.present?
   end
 
   def sent_validation
     # Form
-    errors[:siret] << "Vous devez renseigner un SIRET d'organisation valide avant de continuer" unless nom_raison_sociale.present?
+    errors[:siret] << "Vous devez renseigner un SIRET d’organisation valide avant de continuer" unless nom_raison_sociale.present?
 
     # Homologation de securite
-    errors[:autorite_homologation_nom] << "Vous devez renseigner le nom de l'autorité d'homologation avant de continuer" unless additional_content&.fetch('autorite_homologation_nom', false)&.present?
-    errors[:autorite_homologation_fonction] << "Vous devez renseigner la fonction de l'autorité d'homologation avant de continuer" unless additional_content&.fetch('autorite_homologation_fonction', false)&.present?
+    errors[:autorite_homologation_nom] << "Vous devez renseigner le nom de l’autorité d’homologation avant de continuer" unless additional_content&.fetch('autorite_homologation_nom', false)&.present?
+    errors[:autorite_homologation_fonction] << "Vous devez renseigner la fonction de l’autorité d’homologation avant de continuer" unless additional_content&.fetch('autorite_homologation_fonction', false)&.present?
     errors[:date_homologation] << "Vous devez renseigner la date de début de l’homologation avant de continuer" unless additional_content&.fetch('date_homologation', false)&.present?
     errors[:date_fin_homologation] << "Vous devez renseigner la date de fin de l’homologation avant de continuer" unless additional_content&.fetch('date_fin_homologation', false)&.present?
     errors[:documents_attributes] << "Vous devez joindre le document de décision d’homologation avant de continuer" unless documents.where(type: "Document::DecisionHomologation").present?
@@ -35,9 +35,9 @@ class Enrollment::ApiImpotParticulierStep2 < Enrollment
 
     unless user.email_verified
       errors[:base] << "Vous devez activer votre compte api.gouv.fr avant de continuer.
-Merci de cliquer sur le lien d'activation que vous avez reçu par mail.
-Vous pouvez également demander un nouveau lien d'activation en cliquant sur le lien
-suivant #{ENV.fetch("OAUTH_HOST")}/users/send-email-verification?notification=email_verification_required"
+      Merci de cliquer sur le lien d’activation que vous avez reçu par mail.
+      Vous pouvez également demander un nouveau lien d’activation en cliquant sur le lien
+      suivant #{ENV.fetch("OAUTH_HOST")}/users/send-email-verification?notification=email_verification_required"
     end
   end
 end
