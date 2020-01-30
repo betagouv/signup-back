8429dd4d6475256a5806dbafbfecb83a6fd8e169
Raphaël Dubigny <rdubigny@gmail.com>
2020-01-30 16:29:52 +0100

    improve api entreprise bridge compliance

--- app/models/enrollment/api_entreprise.rb
+++ app/models/enrollment/api_entreprise.rb
@@ -1,2 +1,21 @@
 class Enrollment::ApiEntreprise < Enrollment
+  protected
+
+  def sent_validation
+    super
+
+    email_regex = URI::MailTo::EMAIL_REGEXP
+    # loose homemade regexp to match large amount of phone number
+    phone_number_regex = /^\+?(?:[0-9][ -]?){6,14}[0-9]$/
+
+    contact_technique = contacts&.find { |e| e["id"] == "technique" }
+    errors[:contacts] << "Vous devez renseigner un email valide pour le contact technique avant de continuer" unless email_regex.match?(contact_technique&.fetch("email", false))
+    errors[:contacts] << "Vous devez renseigner un numéro de téléphone valide pour le contact technique avant de continuer" unless phone_number_regex.match?(contact_technique&.fetch("phone_number", false))
+
+    contact_metier = contacts&.find { |e| e["id"] == "metier" }
+    errors[:contacts] << "Vous devez renseigner un email valide le contact métier avant de continuer" unless email_regex.match?(contact_metier&.fetch("email", false))
+    errors[:contacts] << "Vous devez renseigner un numéro de téléphone valide pour le contact métier avant de continuer" unless phone_number_regex.match?(contact_metier&.fetch("phone_number", false))
+
+    errors[:scopes] << "Vous devez cocher au moins un périmètre de donnée avant de continuer" unless scopes.any? { |_, v| v }
+  end
 end
