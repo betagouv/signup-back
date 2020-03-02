84f41ca0508b5a74c80483c97f78e1269969104b
Raphaël Dubigny <rdubigny@gmail.com>
2020-03-02 14:37:50 +0100

    ordered errors + fix typo

--- app/models/enrollment/api_particulier.rb
+++ app/models/enrollment/api_particulier.rb
@@ -4,6 +4,11 @@ class Enrollment::ApiParticulier < Enrollment
   def sent_validation
     super
 
+    errors[:scopes] << "Vous devez cocher au moins un périmètre de données avant de continuer" unless scopes.any? { |_, v| v }
+
+    errors[:data_retention_period] << "Vous devez renseigner la conservation des données avant de continuer" unless data_retention_period.present?
+    errors[:data_recipients] << "Vous devez renseigner les destinataires des données avant de continuer" unless data_recipients.present?
+
     email_regex = URI::MailTo::EMAIL_REGEXP
     # loose homemade regexp to match large amount of phone number
     phone_number_regex = /^\+?(?:[0-9][ -]?){6,14}[0-9]$/
@@ -15,10 +20,5 @@ class Enrollment::ApiParticulier < Enrollment
     contact_metier = contacts&.find { |e| e["id"] == "metier" }
     errors[:contacts] << "Vous devez renseigner un email valide le contact métier avant de continuer" unless email_regex.match?(contact_metier&.fetch("email", false))
     errors[:contacts] << "Vous devez renseigner un numéro de téléphone valide pour le contact métier avant de continuer" unless phone_number_regex.match?(contact_metier&.fetch("phone_number", false))
-
-    errors[:scopes] << "Vous devez cocher au moins un périmètre de donnée avant de continuer" unless scopes.any? { |_, v| v }
-
-    errors[:data_retention_period] << "Vous devez renseigner la conservation des données avant de continuer" unless data_retention_period.present?
-    errors[:data_recipients] << "Vous devez renseigner les destinataires des données avant de continuer" unless data_recipients.present?
   end
 end
--- app/models/enrollment/franceconnect.rb
+++ app/models/enrollment/franceconnect.rb
@@ -4,6 +4,11 @@ class Enrollment::Franceconnect < Enrollment
   def sent_validation
     super
 
+    errors[:scopes] << "Vous devez cocher au moins un périmètre de données avant de continuer" unless scopes.any? { |_, v| v }
+
+    errors[:data_retention_period] << "Vous devez renseigner la conservation des données avant de continuer" unless data_retention_period.present?
+    errors[:data_recipients] << "Vous devez renseigner les destinataires des données avant de continuer" unless data_recipients.present?
+
     email_regex = URI::MailTo::EMAIL_REGEXP
     # loose homemade regexp to match large amount of phone number
     phone_number_regex = /^\+?(?:[0-9][ -]?){6,14}[0-9]$/
@@ -11,10 +16,5 @@ class Enrollment::Franceconnect < Enrollment
     contact_technique = contacts&.find { |e| e["id"] == "technique" }
     errors[:contacts] << "Vous devez renseigner un email valide pour le contact technique avant de continuer" unless email_regex.match?(contact_technique&.fetch("email", false))
     errors[:contacts] << "Vous devez renseigner un numéro de téléphone valide pour le contact technique avant de continuer" unless phone_number_regex.match?(contact_technique&.fetch("phone_number", false))
-
-    errors[:scopes] << "Vous devez cocher au moins un périmètre de donnée avant de continuer" unless scopes.any? { |_, v| v }
-
-    errors[:data_retention_period] << "Vous devez renseigner la conservation des données avant de continuer" unless data_retention_period.present?
-    errors[:data_recipients] << "Vous devez renseigner les destinataires des données avant de continuer" unless data_recipients.present?
   end
 end
