966c93e9d4564da03fe9f97bc7cea9afa9b03970
Raphaël Dubigny <rdubigny@gmail.com>
2020-10-02 17:12:43 +0200

    feat: add validation for year and access mode selection

--- app/models/enrollment/dgfip/api_impot_particulier_fc_sandbox.rb
+++ app/models/enrollment/dgfip/api_impot_particulier_fc_sandbox.rb
@@ -1,2 +1,11 @@
 class Enrollment::ApiImpotParticulierFcSandbox < Enrollment::Dgfip::SandboxEnrollment
+  protected
+
+  def sent_validation
+    super
+
+    unless scopes.any? { |k, v| v && %w[dgfip_annee_n_moins_1 dgfip_annee_n_moins_2].include?(k) }
+      errors[:scopes] << "Vous devez cocher au moins une année de revenus souhaitée avant de continuer"
+    end
+  end
 end
--- app/models/enrollment/dgfip/api_impot_particulier_sandbox.rb
+++ app/models/enrollment/dgfip/api_impot_particulier_sandbox.rb
@@ -1,2 +1,14 @@
 class Enrollment::ApiImpotParticulierSandbox < Enrollment::Dgfip::SandboxEnrollment
+  protected
+
+  def sent_validation
+    super
+
+    unless scopes.any? { |k, v| v && %w[dgfip_annee_n_moins_1 dgfip_annee_n_moins_2].include?(k) }
+      errors[:scopes] << "Vous devez cocher au moins une année de revenus souhaitée avant de continuer"
+    end
+    unless scopes.any? { |k, v| v && %w[dgfip_acces_spi dgfip_acces_etat_civil].include?(k) }
+      errors[:scopes] << "Vous devez cocher au moins une modalité d'accès avant de continuer"
+    end
+  end
 end
