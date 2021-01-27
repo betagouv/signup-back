6f8d1afe90f666f2349006d8a51adc07be7aadb6
Raphaël Dubigny <rdubigny@gmail.com>
2021-01-27 14:34:46 +0100

    fix: unable to only chose dgfip_annee_n_moins_3

--- app/models/enrollment/dgfip/api_impot_particulier_fc_sandbox.rb
+++ app/models/enrollment/dgfip/api_impot_particulier_fc_sandbox.rb
@@ -4,7 +4,7 @@ class Enrollment::ApiImpotParticulierFcSandbox < Enrollment::Dgfip::SandboxEnrol
   def sent_validation
     super
 
-    unless scopes.any? { |k, v| v && %w[dgfip_annee_n_moins_1 dgfip_annee_n_moins_2].include?(k) }
+    unless scopes.any? { |k, v| v && %w[dgfip_annee_n_moins_1 dgfip_annee_n_moins_2 dgfip_annee_n_moins_3].include?(k) }
       errors[:scopes] << "Vous devez cocher au moins une année de revenus souhaitée avant de continuer"
     end
   end
