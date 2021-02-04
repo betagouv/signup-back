3c1659dc2e24c07d4a2de6a95778f8f4daa65961
Raphaël Dubigny <rdubigny@gmail.com>
2021-02-04 11:10:28 +0100

    feat: make date integration a mandatory field

--- app/models/enrollment/francerelance_fc.rb
+++ app/models/enrollment/francerelance_fc.rb
@@ -5,6 +5,7 @@ class Enrollment::FrancerelanceFc < Enrollment
     super
 
     errors[:scopes] << "Vous devez cocher au moins un périmètre de données avant de continuer" unless scopes.any? { |_, v| v }
+    errors[:date_integration] << "Vous devez renseigner une date prévisionnelle de fin d’intégration valide avant de continuer" unless additional_content&.fetch("date_integration", false)&.present?
 
     contact_validation("metier", "porteur de projet")
     contact_validation("technique", "responsable technique")
