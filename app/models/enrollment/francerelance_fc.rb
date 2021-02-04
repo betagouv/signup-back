54ca813071a8ebfdb11611e9725a3602de82201b
Raphaël Dubigny <rdubigny@gmail.com>
2021-02-04 12:14:18 +0100

    fix: on Safari and IE11 dates can at the wrong format

--- app/models/enrollment/francerelance_fc.rb
+++ app/models/enrollment/francerelance_fc.rb
@@ -5,7 +5,9 @@ class Enrollment::FrancerelanceFc < Enrollment
     super
 
     errors[:scopes] << "Vous devez cocher au moins un périmètre de données avant de continuer" unless scopes.any? { |_, v| v }
-    errors[:date_integration] << "Vous devez renseigner une date prévisionnelle de fin d’intégration valide avant de continuer" unless additional_content&.fetch("date_integration", false)&.present?
+
+    date_regex = /^\d{4}-\d{2}-\d{2}$/
+    errors[:date_integration] << "Vous devez renseigner la date prévisionnelle de fin d’intégration au format AAAA-MM-JJ avant de continuer" unless date_regex.match?(additional_content&.fetch("date_integration", false))
 
     contact_validation("metier", "porteur de projet")
     contact_validation("technique", "responsable technique")
