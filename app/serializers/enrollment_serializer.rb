a4e69def3adc812361f9b1fee44f53f58440a167
Raphaël Dubigny <rdubigny@gmail.com>
2021-04-20 12:03:57 +0200

    feat: show nom_raison_sociale by default

--- app/serializers/enrollment_serializer.rb
+++ app/serializers/enrollment_serializer.rb
@@ -1,8 +1,8 @@
 class EnrollmentSerializer < ActiveModel::Serializer
   attributes :updated_at, :created_at, :id, :target_api, :previous_enrollment_id, :copied_from_enrollment_id,
-    :cgu_approved, :scopes, :contacts, :organization_id, :siret, :status, :linked_token_manager_id, :additional_content,
-    :intitule, :description, :fondement_juridique_title, :fondement_juridique_url, :data_recipients,
-    :data_retention_period, :data_retention_comment, :dpo_label, :dpo_email, :dpo_phone_number,
+    :cgu_approved, :scopes, :contacts, :organization_id, :siret, :nom_raison_sociale, :status, :linked_token_manager_id,
+    :additional_content, :intitule, :description, :fondement_juridique_title, :fondement_juridique_url,
+    :data_recipients, :data_retention_period, :data_retention_comment, :dpo_label, :dpo_email, :dpo_phone_number,
     :responsable_traitement_label, :responsable_traitement_email, :responsable_traitement_phone_number, :demarche
 
   belongs_to :user, serializer: UserWithProfileSerializer
