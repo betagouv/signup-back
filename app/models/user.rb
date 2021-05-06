46a40c36f5e901fc37db9d964fc11c775f6a6fd4
Loïc Delmaire <loic.delmaire@gmail.com>
2021-05-06 17:17:43 +0200

    refactor: User.reconcile

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,22 @@ class User < ActiveRecord::Base
 
   def self.reconcile(external_user_info)
     user = where(
-      email: external_user_info["email"]
+      email: external_user_info['email'],
     ).first_or_create!
 
     # the following data must be used as a cache (do not modify them, use fresh data form api-auth whenever you can)
-    user.update(uid: external_user_info["sub"]) if external_user_info.key?("sub")
-    user.update(email_verified: external_user_info["email_verified"]) if external_user_info.key?("email_verified")
-    user.update(family_name: external_user_info["family_name"]) if external_user_info.key?("family_name")
-    user.update(given_name: external_user_info["given_name"]) if external_user_info.key?("given_name")
-    user.update(phone_number: external_user_info["phone_number"]) if external_user_info.key?("phone_number")
-    user.update(job: external_user_info["job"]) if external_user_info.key?("job")
-    user.update(organizations: external_user_info["organizations"]) if external_user_info.key?("organizations")
+    user.uid = external_user_info["sub"] if external_user_info.key?("sub")
+    user.assign_attributes(
+      external_user_info.slice(
+        'email_verified',
+        'family_name',
+        'given_name',
+        'phone_number',
+        'job',
+        'organizations',
+      )
+    )
+    user.save
 
     user
   end
