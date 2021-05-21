3fd74af71cc5c9638c9dc6e1103bb3557d2f17e9
Loïc Delmaire <loic.delmaire@gmail.com>
2021-05-21 10:00:44 +0200

    test: add tests for users index

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,4 +18,65 @@ RSpec.describe UsersController, type: :controller do
       expect(user.attributes).to include(JSON.parse(response.body))
     end
   end
+
+  describe "#index" do
+    subject(:users_index) do
+      get :index, params: {
+        users_with_roles_only: users_with_roles_only
+      }
+    end
+
+    let(:users_with_roles_only) { nil }
+
+    before do
+      login(user)
+    end
+
+    context "when user is not an admin" do
+      let(:user) { create(:user) }
+
+      it { is_expected.to have_http_status(:forbidden) }
+    end
+
+    context "when user is an admin" do
+      let(:user) { create(:user, :administrator) }
+
+      it { is_expected.to have_http_status(:ok) }
+
+      describe "payload" do
+        subject(:users_index_payload) do
+          get :index, params: {
+            users_with_roles_only: users_with_roles_only
+          }
+
+          JSON.parse(response.body)["users"]
+        end
+
+        let!(:first_user_with_role) { create(:user, email: "a#{generate(:email)}", roles: ["whatever"]) }
+        let!(:second_user_without_role) { create(:user, email: "b#{generate(:email)}", roles: []) }
+        it "renders all users ordered by email" do
+          expect(users_index_payload.count).to eq(3)
+
+          expect(users_index_payload.map { |user_payload| user_payload["id"] }).to eq([
+            first_user_with_role.id,
+            second_user_without_role.id,
+            user.id
+          ])
+        end
+
+        context "when users_with_roles_only is set to true" do
+          let(:users_with_roles_only) { true }
+
+          it "renders users with roles only" do
+            expect(users_index_payload.count).to eq(2)
+
+            expect(users_index_payload.map { |user_payload| user_payload["id"] }).to eq([
+              first_user_with_role.id,
+              user.id
+            ])
+          end
+        end
+      end
+    end
+  end
 end
