fa07502bbf4979df789b9f7160bf0580a76187bb
Raphaël Dubigny <rdubigny@gmail.com>
2021-06-07 15:22:59 +0200

    feat: update aidants connect

--- app/bridges/aidants_connect_bridge.rb
+++ app/bridges/aidants_connect_bridge.rb
@@ -6,7 +6,10 @@ class AidantsConnectBridge < ApplicationBridge
         data_pass_id: @enrollment.id,
         organization_name: @enrollment.intitule,
         organization_siret: @enrollment.siret,
-        organization_address: @enrollment.additional_content&.fetch("organization_address", "")
+        organization_address: @enrollment.additional_content&.fetch("organization_address", ""),
+        organization_postal_code: @enrollment.additional_content&.fetch("organization_postal_code", ""),
+        organization_city: @enrollment.additional_content&.fetch("organization_city", ""),
+        organization_type: @enrollment.additional_content&.fetch("organization_type", "")
       },
       ENV.fetch("AIDANTS_CONNECT_API_KEY"),
       "Aidants Connect"
