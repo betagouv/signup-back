2145dfda58633f03dcf1b4818f6837a343609573
Raphaël Dubigny <rdubigny@gmail.com>
2021-06-24 16:31:53 +0200

    feat: remove contact metier from api particulier

--- app/models/enrollment/api_particulier.rb
+++ app/models/enrollment/api_particulier.rb
@@ -7,6 +7,5 @@ class Enrollment::ApiParticulier < Enrollment
     errors[:scopes] << "Vous devez cocher au moins un périmètre de données avant de continuer" unless scopes.any? { |_, v| v }
 
     contact_technique_validation
-    contact_metier_validation
   end
 end
