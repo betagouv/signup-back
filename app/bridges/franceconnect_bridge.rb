d4534e331ac340848556c601f019af8b266f350d
Raphaël Dubigny <rdubigny@gmail.com>
2021-07-08 14:20:46 +0200

    feat: udpate fc bridge

--- app/bridges/franceconnect_bridge.rb
+++ app/bridges/franceconnect_bridge.rb
@@ -1,23 +1,27 @@
 class FranceconnectBridge < ApplicationBridge
   def call
     nom_raison_sociale = @enrollment.nom_raison_sociale
+    intitule = @enrollment.intitule
     email = @enrollment.contacts.find { |contact| contact["id"] == "technique" }["email"]
     scopes = @enrollment[:scopes].reject { |k, v| !v }.keys
     eidas_level = @enrollment.additional_content&.fetch("eidas_level", "")
-    create_enrollment_in_token_manager(@enrollment.id, nom_raison_sociale, email, scopes, eidas_level)
+    create_enrollment_in_token_manager(@enrollment.id, intitule, nom_raison_sociale, email, scopes, eidas_level)
   end
 
   private
 
-  def create_enrollment_in_token_manager(id, nom_raison_sociale, email, scopes, eidas_level)
+  def create_enrollment_in_token_manager(id, intitule, nom_raison_sociale, email, scopes, eidas_level)
     if eidas_level == "1"
       # note that the FC team test this call with this bash script: https://gitlab.com/france-connect/FranceConnect/snippets/1828712
       response = Http.post(
         "#{ENV.fetch("FRANCECONNECT_PARTICULIER_HOST")}/api/v2/service-provider/integration/create",
         {
           name: "#{nom_raison_sociale} - #{id}",
+          service_name: intitule,
+          corporate_name: nom_raison_sociale,
           authorized_emails: [email],
           signup_id: id,
+          datapass_id: id,
           scopes: scopes
         },
         ENV.fetch("FRANCECONNECT_PARTICULIER_API_KEY"),
