97bb1d6e16a7d6eec6092ec8e701d03b8ae5a6a4
Loïc Delmaire <loic.delmaire@gmail.com>
2021-08-05 18:32:53 +0200

    feat: Introduce serializers for webhooks
    The main serializer (`WebhookSerializer`) does not inherit from
    ActiveModel::Serializer in order to build a flat payload.
    
    WebhookEnrollmentSerializer is almost the same as EnrollmentSerializer,
    but with some sanitized fields and associations.

---
+++ app/serializers/webhook_enrollment_serializer.rb
@@ -0,0 +1,36 @@
+class WebhookEnrollmentSerializer < ActiveModel::Serializer
+  attributes :id,
+    :target_api,
+    :intitule,
+    :description,
+    :fondement_juridique_title,
+    :fondement_juridique_url,
+    :cgu_approved,
+    :additional_content,
+    :type_projet,
+    :data_recipients,
+    :data_retention_period,
+    :data_retention_comment,
+    :date_mise_en_production,
+    :status,
+    :data_retention_period,
+    :data_retention_comment,
+    :siret,
+    :nom_raison_sociale,
+    :organization_id,
+    :contacts,
+    :scopes,
+    :updated_at,
+    :created_at,
+    :previous_enrollment_id,
+    :copied_from_enrollment_id,
+    :linked_token_manager_id,
+    :volumetrie_approximative,
+    :dpo_is_informed
+
+  belongs_to :user, serializer: UserWithProfileSerializer
+  belongs_to :dpo, serializer: UserWithProfileSerializer
+  belongs_to :responsable_traitement, serializer: UserWithProfileSerializer
+
+  has_many :events, serializer: WebhookEventSerializer
+end
