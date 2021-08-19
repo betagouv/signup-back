82be5a717141a6582b5f9cbbbf6706b483a2a894
Loïc Delmaire <loic.delmaire@gmail.com>
2021-08-19 10:41:07 +0200

    feat: ApiEntrepriseNotifier delivers emails like BaseNotifier
    We enable this feature to allow a smooth transition from webhooks to
    emails, and to perform end to end testing between apps.

--- app/notifiers/api_entreprise_notifier.rb
+++ app/notifiers/api_entreprise_notifier.rb
@@ -4,6 +4,7 @@ class ApiEntrepriseNotifier < AbstractNotifier
 
   def created
     deliver_event_webhook(__method__)
+    deliver_created_mail_to_enrollment_creator if default_mailer_active?
   end
 
   def updated(diff:, user_id:)
@@ -22,24 +23,29 @@ class ApiEntrepriseNotifier < AbstractNotifier
 
   def send_application(comment:, current_user:)
     deliver_event_webhook(__method__)
+    deliver_event_mailer(__method__, comment) if default_mailer_active?
 
     notify_subscribers_by_email_for_sent_application(current_user: current_user)
   end
 
   def notify(comment:, current_user:)
     deliver_event_webhook(__method__)
+    deliver_event_mailer(__method__, comment) if default_mailer_active?
   end
 
   def review_application(comment:, current_user:)
     deliver_event_webhook(__method__)
+    deliver_event_mailer(__method__, comment) if default_mailer_active?
   end
 
   def refuse_application(comment:, current_user:)
     deliver_event_webhook(__method__)
+    deliver_event_mailer(__method__, comment) if default_mailer_active?
   end
 
   def validate_application(comment:, current_user:)
     deliver_event_webhook(__method__)
+    deliver_event_mailer(__method__, comment) if default_mailer_active?
 
     if enrollment.responsable_traitement.present?
       deliver_rgpd_email_for(:responsable_traitement)
@@ -48,5 +54,11 @@ class ApiEntrepriseNotifier < AbstractNotifier
     if enrollment.dpo.present?
       deliver_rgpd_email_for(:dpo)
     end
+  end
+
+  private
+
+  def default_mailer_active?
+    ENV["API_ENTREPRISE_MAILER_OFF"].nil?
   end
 end
