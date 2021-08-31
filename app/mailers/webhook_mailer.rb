2b96bccc2f5f2d12fd07a8aba1344d6551c7ef0c
Raphaël Dubigny <rdubigny@gmail.com>
2021-08-31 17:08:17 +0200

    feat: improve error email when webhook is not responding

--- app/mailers/webhook_mailer.rb
+++ app/mailers/webhook_mailer.rb
@@ -6,9 +6,10 @@ class WebhookMailer < ActionMailer::Base
     @payload = JSON.pretty_generate(params[:payload])
     @webhook_response_body = params[:webhook_response_body]
     @webhook_response_status = params[:webhook_response_status]
+    @api_manager_label = api_manager_label
 
     mail(
-      subject: "[Datapass] Votre webhook endpoint est en défaut",
+      subject: "[Datapass] Erreur de communication avec votre #{api_manager_label}",
       to: target_api_instructor_emails,
       from: "datapass@api.gouv.fr",
       cc: "datapass@api.gouv.fr"
@@ -19,5 +20,13 @@ class WebhookMailer < ActionMailer::Base
 
   def target_api_instructor_emails
     User.where("'#{params[:target_api]}:instructor' = ANY(roles)").pluck(:email)
+  end
+
+  def api_manager_label
+    target_api_data["api_manager_label"]
+  end
+
+  def target_api_data
+    DataProvidersConfiguration.instance.config_for(params[:target_api])
   end
 end
