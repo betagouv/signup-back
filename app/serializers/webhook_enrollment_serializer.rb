92dc0d596050004747c5bad47c82114f105d8067
Loïc Delmaire <loic.delmaire@gmail.com>
2021-08-31 16:58:27 +0200

    feat: Simplify WebhookEnrollmentSerializer
    Keep it simple for now, remove non relevant fields for API Entreprise
    (which is the only target api which currently uses webhooks)
    
    Moreover it adopts the next interface which introduces team members on
    enrollments.

--- app/serializers/webhook_enrollment_serializer.rb
+++ app/serializers/webhook_enrollment_serializer.rb
@@ -1,37 +1,61 @@
 class WebhookEnrollmentSerializer < ActiveModel::Serializer
   attributes :id,
-    :target_api,
     :intitule,
     :description,
-    :fondement_juridique_title,
-    :fondement_juridique_url,
-    :cgu_approved,
-    :additional_content,
-    :type_projet,
-    :data_recipients,
-    :data_retention_period,
-    :data_retention_comment,
-    :date_mise_en_production,
     :status,
-    :data_retention_period,
-    :data_retention_comment,
     :siret,
-    :nom_raison_sociale,
-    :organization_id,
-    :contacts,
     :scopes,
-    :updated_at,
-    :created_at,
+    :team_members,
     :previous_enrollment_id,
-    :copied_from_enrollment_id,
-    :linked_token_manager_id,
-    :volumetrie_approximative,
-    :dpo_is_informed
+    :copied_from_enrollment_id
 
-  belongs_to :user, serializer: WebhookEnrollmentOwnerSerializer
-  belongs_to :dpo, serializer: UserWithProfileSerializer
-  belongs_to :responsable_traitement, serializer: UserWithProfileSerializer
+  def team_members
+    [
+      build_team_member(:user, "demandeur"),
+      build_team_member(:dpo, "delegue_protection_donnees"),
+      build_team_member(:responsable_traitement, "responsable_traitement"),
+      build_team_member_from_contact(:technique, "responsable_technique"),
+      build_team_member_from_contact(:metier, "contact_metier")
+    ].compact
+  end
 
   has_many :events, serializer: WebhookEventSerializer
-  has_many :documents
+
+  private
+
+  def build_team_member(model_key, type)
+    model = object.public_send(model_key)
+
+    return if model.blank?
+
+    {
+      id: model.id,
+      uid: model.try(:uid),
+      type: type,
+      email: model.email,
+      family_name: model.family_name,
+      given_name: model.given_name,
+      phone_number: model.phone_number,
+      job: model.job
+    }
+  end
+
+  def build_team_member_from_contact(original_contact_id, type)
+    contact_payload = object.contacts.find do |contact|
+      contact["id"] == original_contact_id.to_s
+    end
+
+    return if contact_payload.nil?
+
+    {
+      id: contact_payload["email"].to_i(36),
+      uid: nil,
+      type: type,
+      email: contact_payload["email"],
+      family_name: contact_payload["family_name"],
+      givan_name: contact_payload["given_name"],
+      phone_number: contact_payload["phone_number"],
+      job: contact_payload["job"]
+    }
+  end
 end
