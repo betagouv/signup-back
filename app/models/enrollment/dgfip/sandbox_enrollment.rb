86f2dd11d6c827d7be2991ebb894be12a7abc783
Raphaël Dubigny <rdubigny@gmail.com>
2021-09-10 10:29:38 +0200

    feat: get back rgpd_general_agreement

--- app/models/enrollment/dgfip/sandbox_enrollment.rb
+++ app/models/enrollment/dgfip/sandbox_enrollment.rb
@@ -7,6 +7,7 @@ class Enrollment::Dgfip::SandboxEnrollment < Enrollment
     errors[:description] << "Vous devez renseigner la description de la démarche avant de continuer" unless description.present?
     errors[:siret] << "Vous devez renseigner un SIRET d’organisation valide avant de continuer" unless nom_raison_sociale
     errors[:cgu_approved] << "Vous devez valider les modalités d’utilisation avant de continuer" unless cgu_approved?
+    errors[:rgpd_general_agreement] << "Vous devez attester respecter les principes RGPD avant de continuer" unless additional_content&.fetch("rgpd_general_agreement", false)
 
     responsable_technique_validation
   end
